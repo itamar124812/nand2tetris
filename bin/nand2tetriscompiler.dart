6a7b9e4667e8c9609a32f2bc80008401ba6c124d
itamar124812 <itamar124812@gmail.com>
2022-03-09 23:18:56 +0200

    add

---
+++ bin/nand2tetriscompiler.dart
@@ -0,0 +1,5 @@
+import 'package:nand2tetriscompiler/nand2tetriscompiler.dart' as nand2tetriscompiler;
+
+void main(List<String> arguments) {
+  print('Hello world: ${nand2tetriscompiler.calculate()}!');
+}
---
+++ test/nand2tetriscompiler_test.dart
@@ -0,0 +1,8 @@
+import 'package:nand2tetriscompiler/nand2tetriscompiler.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
