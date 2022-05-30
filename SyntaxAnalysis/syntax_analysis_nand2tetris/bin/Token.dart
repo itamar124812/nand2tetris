39e5e00432815b7a94d0b4c0ef237fa075078fa3
itamar124812 <itamar124812@gmail.com>
2022-05-30 20:24:09 +0300

    message

--- SyntaxAnalysis/syntax_analysis_nand2tetris/bin/Token.dart
+++ SyntaxAnalysis/syntax_analysis_nand2tetris/bin/Token.dart
@@ -1,7 +1,21 @@
+import 'dart:io';
+ enum TokenType
+  {
+    keyword,
+    string,
+    intager,
+    identifier,
+    symbol
+  }
 class Token
 {
+ 
    Token(String Path)
    {
+     File f=File(Path);
+
      
+
    }
+   
 }
