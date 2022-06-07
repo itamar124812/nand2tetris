53d9df30a0aabac9416a4a4c15879a7322a1917b
itamar124812 <itamar124812@gmail.com>
2022-06-08 00:18:47 +0300

    Symbol

---
+++ code_gneretor/bin/symbol.dart
@@ -0,0 +1,15 @@
+enum category
+{
+  static,field,argument,local
+}
+class Symbol
+{
+  String Name;
+  String Type;
+  category cat;
+  int counter;
+  Symbol(String _name,String _type,category _cat,int _counter ):Name=_name, Type=_type,cat=_cat,counter=_counter
+  {
+
+  }
+}
