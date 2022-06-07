6b69ab19bdcea5b15b022983fe22ff754932e9cb
itamar124812 <itamar124812@gmail.com>
2022-06-08 01:34:01 +0300

    Sat

--- code_gneretor/bin/code_gneretor.dart
+++ code_gneretor/bin/code_gneretor.dart
@@ -2,11 +2,9 @@ import 'dart:ffi';
 import 'dart:io';
 
 
-/*void main(List<String> arguments) {
-  print('Hello world: ${code_gneretor.calculate()}!');
-}*/
-
-class code_genretor {
- 
+void main(List<String> arguments) {
+  
 }
+
+
 
