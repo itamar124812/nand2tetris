e147087ef1cf76894ce5611b6769da2777d15bbf
itamar124812 <itamar124812@gmail.com>
2022-06-08 02:35:06 +0300

    Sat

--- code_gneretor/bin/codeGen.dart
+++ code_gneretor/bin/codeGen.dart
@@ -1,3 +1,48 @@
+import 'dart:io';
+
+import 'Table.dart';
+
 class code_genretor {
- 
+  StringBuffer output=StringBuffer();
+  SymbolTable symbolTable=SymbolTable();
+  int index=2;
+  String _filename="";
+  String _className="";
+  int varlibels=0;
+   var inputFile;
+  code_genretor(String path)
+  {
+    var outputFile = File(
+    path.substring(0, path.lastIndexOf(r"\")) + r"\" + path.split("\\").last.split(".").first + ".vm");
+    inputFile = File(path).readAsLinesSync();
+    outputFile.create(recursive: true).then((File outputFile) {});
+    classf();
+  }
+  
+ void classf()
+ {
+    _className=inputFile[index++];
+    if(_className!="Main") output.writeln("function $_className.new 0"); 
+    while(inputFile[imdex]==r"(field|static)"){        
+        varlibels+=1;
+        
+        break;     
+      }
+      case "static":
+      {
+        
+        
+        break;
+        }    
+    case "function":
+    {
+
+    }
+    case "constructor":
+
+   {
+
+   }
+    }
+ }
 }
