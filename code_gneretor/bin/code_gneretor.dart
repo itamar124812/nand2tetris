df7e6d3ee688baf8ad24048e329b5754ea43a2b8
itamar124812 <itamar124812@gmail.com>
2022-06-07 23:59:04 +0300

    code gneretor init

---
+++ code_gneretor/bin/code_gneretor.dart
@@ -0,0 +1,5 @@
+import 'package:code_gneretor/code_gneretor.dart' as code_gneretor;
+
+void main(List<String> arguments) {
+  print('Hello world: ${code_gneretor.calculate()}!');
+}
---
+++ code_gneretor/test/code_gneretor_test.dart
@@ -0,0 +1,8 @@
+import 'package:code_gneretor/code_gneretor.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
