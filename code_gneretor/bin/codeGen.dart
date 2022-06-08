a244869cb448719c253aeb027200a6832aba0ea3
itamar124812 <itamar124812@gmail.com>
2022-06-08 19:22:58 +0300

    Sat

--- code_gneretor/bin/codeGen.dart
+++ code_gneretor/bin/codeGen.dart
@@ -1,24 +1,44 @@
 import 'dart:io';
+
+
 import 'Table.dart';
 import 'symbol.dart';
 
 class code_genretor {
   StringBuffer output=StringBuffer();
   SymbolTable symbolTable=SymbolTable();
+  
   int index=2;
   String _filename = "";
   String _className = "";
   int varlibels = 0;
-   var inputFile;
+  List<String> inputFile=[];
+  String stringinput="";
+  
   code_genretor(String path)
   {
     var outputFile = File(
     path.substring(0, path.lastIndexOf(r"\")) + r"\" + path.split("\\").last.split(".").first + ".vm");
     inputFile = File(path).readAsLinesSync();
+    stringinput = File(path).readAsStringSync();
     outputFile.create(recursive: true).then((File outputFile) {});
+    _filename = path.split("\\").last.split(".").first;
     classf();
   }
-
+String getTok(int index)
+{
+  if(index>=0&&index<inputFile.length)
+  {
+    String str= inputFile![index];
+    var w = str.substring(
+        str.indexOf('>') + 2, str.indexOf('<', str.indexOf('>') + 1) - 1);
+    return w;
+  }
+  else
+  {
+    return "";
+  }
+}
   String currentTok() {
     String str = inputFile![index];
     var w = str.substring(
@@ -28,15 +48,22 @@ class code_genretor {
 
   int findClassVarDec() {
     int temp = index;
-    while(!inputFile[temp].contains("<classVarDec>") || !inputFile[temp].contains("<subroutineDec>") || !inputFile[temp].contains("}") ) {
-      temp++;
-    }
-    if(inputFile[temp].contains("<subroutineDec>") || inputFile[temp].contains("}")) {
-      return -1;
-    }
-    return temp;
+   while(stringinput.contains("<classVarDec>",temp))
+   {
+      temp=stringinput.indexOf("<classVarDec>",temp);
+      return temp;
+   }
+    return -1;
   }
-
+  int findToken(String token) {
+    int temp = index;
+  if(stringinput.contains(token,temp))
+   {
+      temp=inputFile.indexWhere((String str)=>str.trim().startsWith(token),temp);
+      return temp;
+   }
+    return -1;
+  }
  void classf()
  {
     _className=currentTok();
@@ -59,7 +86,27 @@ class code_genretor {
 
           break;
         }
-
-      }
+    }
+    if(findToken("<subroutineDec>")!=-1){
+      {
+        int varcount=0;
+        SymbolTable subroutineTable=SymbolTable();
+        index=findToken("<subroutineDec>");
+        String funcName=getTok(index+3);
+        while(findToken("<varDec>")!=-1){       
+          index=findToken("<varDec>");
+            String _type=getTok(index+2);
+          subroutineTable.addSymbol(Symbol(getTok(index+3),_type,category.local,++varcount));
+          index+=4;
+          while(currentTok()==","){
+            index++;
+            subroutineTable.addSymbol(Symbol(getTok(index),_type,category.local,++varcount));
+            index++;
+          }        
+       }
+        output.writeln("function $_className.$funcName $varcount");
+       
  }
+    }
+}
 }
--- code_gneretor/bin/code_gneretor.dart
+++ code_gneretor/bin/code_gneretor.dart
@@ -1,9 +1,11 @@
 import 'dart:ffi';
 import 'dart:io';
 
+import 'codeGen.dart';
+
 
 void main(List<String> arguments) {
-  
+  code_genretor(r"C:\Users\USER\nand2tetris\projects\10\ArrayTest\Main.xml");
 }
 
 
