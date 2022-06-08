73a7f99910bf26a6848a9b848694da7caf9e76db
Raz <razaviad@g.jct.ac.il>
2022-06-08 03:09:55 +0300

    field

--- code_gneretor/bin/codeGen.dart
+++ code_gneretor/bin/codeGen.dart
@@ -1,14 +1,14 @@
 import 'dart:io';
-
 import 'Table.dart';
+import 'symbol.dart';
 
 class code_genretor {
   StringBuffer output=StringBuffer();
   SymbolTable symbolTable=SymbolTable();
   int index=2;
-  String _filename="";
-  String _className="";
-  int varlibels=0;
+  String _filename = "";
+  String _className = "";
+  int varlibels = 0;
    var inputFile;
   code_genretor(String path)
   {
@@ -18,31 +18,48 @@ class code_genretor {
     outputFile.create(recursive: true).then((File outputFile) {});
     classf();
   }
-  
+
+  String currentTok() {
+    String str = inputFile![index];
+    var w = str.substring(
+        str.indexOf('>') + 2, str.indexOf('<', str.indexOf('>') + 1) - 1);
+    return w;
+  }
+
+  int findClassVarDec() {
+    int temp = index;
+    while(!inputFile[temp].contains("<classVarDec>") || !inputFile[temp].contains("<subroutineDec>") || !inputFile[temp].contains("}") ) {
+      temp++;
+    }
+    if(inputFile[temp].contains("<subroutineDec>") || inputFile[temp].contains("}")) {
+      return -1;
+    }
+    return temp;
+  }
+
  void classf()
  {
-    _className=inputFile[index++];
-    if(_className!="Main") output.writeln("function $_className.new 0"); 
-    while(inputFile[imdex]==r"(field|static)"){        
-        varlibels+=1;
-        
-        break;     
+    _className=currentTok();
+    index++;
+    if(_className!="Main") output.writeln("function $_className.new 0");
+    if(findClassVarDec() != -1){
+      index = findClassVarDec();
+      while(currentTok()=="static" || currentTok()=="field"){
+          if(currentTok()=="field"){
+            index++;
+            String type = currentTok();
+            index++;
+            symbolTable.addSymbol(Symbol(currentTok(),type,category.field,varlibels++));
+            while(currentTok()==","){
+              index++;
+              symbolTable.addSymbol(Symbol(currentTok(),type,category.field,varlibels++));
+              index++;
+            }
+          }
+
+          break;
+        }
+
       }
-      case "static":
-      {
-        
-        
-        break;
-        }    
-    case "function":
-    {
-
-    }
-    case "constructor":
-
-   {
-
-   }
-    }
  }
 }
