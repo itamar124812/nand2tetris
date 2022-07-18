ec22686c3cfa00c77df623698a4004d7e34fd7e0
Itamar Cohen <72938168+itamar124812@users.noreply.github.com>
2022-07-18 15:43:35 +0300

    Add files via upload

---
+++ vm_translator/bin/vm_translator.dart
@@ -0,0 +1,6 @@
+import 'package:vm_translator/vm_translator.dart' as vm_translator;
+
+void main(List<String> arguments) {
+  var fileName=arguments[1];
+   
+}
---
+++ vm_translator/test/vm_translator_test.dart
@@ -0,0 +1,8 @@
+import 'package:vm_translator/vm_translator.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
